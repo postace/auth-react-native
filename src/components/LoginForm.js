61549173ffa05d327e6d14bb3006895e4bb0ad87
Hai Pham <hai.phamthanh@vndirect.com.vn>
2017-06-03 20:26:18 +0700

    Add password input box and secure it

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,8 @@ import { Card, Button, CardSection, Input } from './common';
 export default class LoginForm extends Component {
 
   state = {
-    email: ''
+    email: '',
+    password: ''
   };
 
   /*By default, text input will not have any width, height*/
@@ -17,6 +18,16 @@ export default class LoginForm extends Component {
             label="Email"
             value={this.state.email}
             onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+          secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
           />
         </CardSection>
 
