afcf6ed1404b7bfddb60c2ac55ba8618d6a23c8d
Hai Pham <hai.phamthanh@vndirect.com.vn>
2017-06-03 14:42:34 +0700

    Add firebase sdk and init firebase before our gui rendering

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import {Header} from './components/common';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Header } from './components/common';
+import firebase from 'firebase';
 
 export default class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyD4cKbv-5WLXkovlL9er9KYyc5wKQsePxU",
+      authDomain: "auth-react-native-86837.firebaseapp.com",
+      databaseURL: "https://auth-react-native-86837.firebaseio.com",
+      projectId: "auth-react-native-86837",
+      storageBucket: "auth-react-native-86837.appspot.com",
+      messagingSenderId: "947417436484"
+    });
+  }
+
   render() {
     return (
       <View>
