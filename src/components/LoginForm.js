9a49e86389362403095a17a1709bc966e4f47d32
Hai Pham <hai.phamthanh@vndirect.com.vn>
2017-06-03 15:00:48 +0700

    Set state for textInput

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
+import { TextInput } from 'react-native';
 import { Card, Button, CardSection } from './common';
 
 export default class LoginForm extends Component {
+
+  state = {
+    text: ''
+  };
+
   /*By default, text input will not have any width, height*/
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput style={{"width": "100%", height: 20}}/>
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({text: text})}
+            style={{ "width": "100%", height: 20 }} />
         </CardSection>
 
         <CardSection>
